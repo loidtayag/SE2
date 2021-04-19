70ac99c2a995e47e841feb177b375383a4cf6bdd
Loid Tayag <loid.tayag@ucdconnect.ie>
2021-04-19 23:47:25 +0100

    nothing

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+struct data {
+    char *player1_name;
+    char *player2_name;
+    char *player1_color;
+    char *player2_color;
+    int player1_score;
+    int player2_score;
+};
+
 int main() {
-    printf("Hello, World!\n");
+    struct data Data;
+
     return 0;
 }

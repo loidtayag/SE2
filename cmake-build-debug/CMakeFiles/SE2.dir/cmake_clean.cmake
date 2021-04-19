6e021814f977e89ef99330ed21d7d54e299ae450
Loid Tayag <loid.tayag@ucdconnect.ie>
2021-04-19 21:33:07 +0100

    setting up files

---
+++ cmake-build-debug/CMakeFiles/SE2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SE2.dir/main.c.obj"
+  "SE2.exe"
+  "SE2.exe.manifest"
+  "SE2.pdb"
+  "libSE2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SE2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

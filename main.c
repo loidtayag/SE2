dffa92ea8b011a0f9573a7a985d9a26d75d4c8cb
Loid Tayag <loidtayag27@gmail.com>
2021-04-30 17:57:12 +0100

    Cleaning

--- main.c
+++ main.c
@@ -4,21 +4,19 @@ int main() {
     bool gameOver = false;
     bool validMove = false;
 
-    /**Initialization**/
+    /*Initialization*/
     InitializeGameSettings();
     PrintTurn();
 
     while (gameOver == false) {
-        do { /**Ensuring validity of move**/
+        /*Ensuring validity of move*/
+        do {
             /*User Input for move*/
             GetMove();
 
             /*Tests for validity of move*/
             if (IsEmptyMove() == true) { //Spot is empty
                 if (DirectionMove() == true) { //At least one direction had at least one disc to be captured
-                    /*Results of the move*/
-                    PrintTurn();
-                    SwitchTurn();
                     validMove = true; //It has passed every test thus making it a valid move, ends loop
                 }
                 else { //No direction had at least one disc to be captured
@@ -28,15 +26,17 @@ int main() {
             else { //Spot isn't empty
                 printf("Invalid move because it's not empty!\n");
             }
-
         } while (validMove == false);
+
+        /*Results of the move*/
+        PrintTurn();
 
         /*Checking if game should end or move on to next player's turn*/
         if (NULL) { //Ending game
-
             gameOver = true;
         }
         else { //Resetting variables for next player's turn
+            SwitchTurn();
             validMove = false;
         }
     }
